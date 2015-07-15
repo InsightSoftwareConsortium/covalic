a72819e339ad5841fc8e9a251b2ab1479edafd10
Marcel Prastawa <marcel.prastawa@gmail.com>
2015-07-15 13:32:24 -0400

    Modified rank aggregation script so it can work for both pre-challenge and post-challenge scenarios. Adjusted handling of missing data entries.

--- Python/RankAggregation/UnsupervisedLearningRankAggregator.py
+++ Python/RankAggregation/UnsupervisedLearningRankAggregator.py
@@ -11,22 +11,24 @@
 #
 
 import numpy as np
+
+import scipy.stats
 import scipy.stats.mstats as mstats
 
 class UnsupervisedLearningRankAggregator:
 
   def __init__(self):
     # Metric precision, number of decimals to be considered for ranking
-    self.decimals = 5
+    self.decimals = 8
 
-    # Threshold for ranks
-    self.threshold = 100
+    # Threshold for ranks, only look at top-k ranks for updates
+    self.threshold = 10
 
-    self.maxIterations = 200
+    self.maxIterations = 100
 
-    self.learningRate = 0.001
+    self.learningRate = 1e-4
 
-    self.tolerance = 1e-4
+    self.tolerance = 1e-6
 
     self.weights = None
 
@@ -42,6 +44,7 @@ class UnsupervisedLearningRankAggregator:
     self.weights = W / W.sum()
 
   def set_threshold(self, t):
+    """Set the threshold for rank updates, only look at top-k ranks."""
     self.threshold = t
 
   def get_rank_vector(self, x):
@@ -50,15 +53,29 @@ class UnsupervisedLearningRankAggregator:
     n = len(x)
 
     if np.all(np.isnan(x)):
-      return np.ones(len(x)) * n
+      #return np.ones(len(x)) * n
+      return np.ones(len(x)) * (self.threshold + 1)
 
     ranks = mstats.rankdata(np.ma.masked_invalid(x))
 
+    maxrank = np.max(ranks)
+
     # Make all missing data have the same rank, not ordered by appearance
+    #ranks[ranks == 0] = maxrank + 1
+    ranks[ranks == 0] = self.threshold + 1
+
+
+    """
     #ranks[ranks == 0] = n + 1
     ranks[ranks == 0] = np.nan
 
-    ranks -= 1
+    # Convert nan to max rank, penalize missing submissions
+    maxrank = np.nanmax(ranks)
+    if np.isnan(maxrank):
+      maxrank = 0 # All values missing, set to equal rank
+
+    ranks[np.isnan(ranks)] = maxrank + 1
+    """
 
     return ranks
 
@@ -78,11 +95,29 @@ class UnsupervisedLearningRankAggregator:
     for metricTable in metricTableList:
 
       rankTable = np.zeros((numSamples, numMetrics), np.float64)
+
       for i in range(numMetrics):
         metricValues = metricTable[:,i].copy() # Smaller value is better
         if metricOrder[i] > 0:
           # Higher value is better
           metricValues *= -1.0
+
+        # Handle missing values (nan) by filling with random values,
+        # drawn from uniform distribution. This reduces reliability measure
+        # of missing metrics.
+        """
+        minv = np.nanmin(metricValues)
+        maxv = np.nanmax(metricValues)
+
+        if np.isnan(maxv) or np.isnan(minv):
+          minv = 0.0
+          maxv = 1.0
+
+        missingInd = np.where(np.isnan(metricValues))
+
+        metricValues[missingInd] = np.random.uniform(minv, maxv,
+          size=len(missingInd[0]))
+        """
 
         # Round the values, consider insignificant bits to be equal
         metricValues = np.around(metricValues, decimals=self.decimals)
@@ -91,11 +126,6 @@ class UnsupervisedLearningRankAggregator:
 
       #print "Ranks", rankTable
 
-      maskedRankTable = rankTable.copy()
-      maskedRankTable[np.isnan(maskedRankTable)] = 0
-
-      rankTable[np.isnan(rankTable)] = numSamples
-
       rankTableList.append(rankTable)
 
     for optIter in range(self.maxIterations):
@@ -103,47 +133,74 @@ class UnsupervisedLearningRankAggregator:
 
       weightUpdates = self.weights.copy()
 
-      for rankTable in rankTableList:
+      gradw = np.zeros(numMetrics)
 
-        #numMissingPerRow = np.sum(np.isnan(rankTable), axis=1)
+      #objF = 0
+
+      for rankTable in rankTableList:
 
         numThresholded = \
           np.sum(rankTable <= self.threshold)
-          #np.sum(maskedRankTable <= self.threshold, axis=1)
-          #np.sum(maskedRankTable <= self.threshold, axis=1) - numMissingPerRow
 
         if numThresholded < 5:
           continue
 
-        # Minimizing w * (r - meanRank)**2
-        meanRank = np.sum(rankTable, axis=1) / numThresholded
-        #meanRank = np.sum(maskedRankTable, axis=1) / numThresholded
+        rankTable[rankTable > self.threshold] = self.threshold + 1
+
+        # Minimizing w * (r - centralRank)**2
+        #centralRank = np.sum(rankTable, axis=1) / numThresholded
+
+        centralRank = np.sum(rankTable, axis=1)
+        centralRank = scipy.stats.rankdata(centralRank)
+
+        #centralRank = np.median(rankTable, axis=1)
+        #centralRank = scipy.stats.rankdata(centralRank)
 
         # Minimizing (r - sum(w * r))**2
-        #meanRank = np.sum(maskedRankTable * self.weights, axis=1)
+        #centralRank = np.sum(rankTable * self.weights, axis=1)
+        #centralRank = scipy.stats.rankdata(centralRank)
 
-        #print "Mean rank", meanRank
+        #print "Mean rank", centralRank
+
+        """
+        delta = np.zeros(numSamples)
+        for i in range(numMetrics):
+          rank_i = rankTable[:,i]
+
+          delta_i = (rank_i - centralRank)
+
+          delta += delta_i
+
+          #objF += np.sum(delta_i ** 2.0)
+        """
 
         for i in range(numMetrics):
           rank_i = rankTable[:,i]
 
-          delta_i = (rank_i - meanRank)
-          delta_i[rank_i > self.threshold] = \
-            self.threshold + 1.0 - meanRank[rank_i > self.threshold]
-
-          delta_i[np.isnan(delta_i)] = 0
+          delta_i = (rank_i - centralRank)
 
           delta_i = delta_i ** 2.0
-
           weightUpdates[i] *= np.exp(-self.learningRate * np.sum(delta_i))
 
-          #weightUpdates[i] -= self.learningRate * np.sum(delta_i) 
+          #ktau, pval = scipy.stats.kendalltau(rank_i, centralRank)
+          #ktau = (ktau + 1) * -1 # Make it so high value means disagreement
+          #weightUpdates[i] *= np.exp(-self.learningRate * ktau)
 
-          #weightUpdates[i] += self.learningRate * np.sum(delta_i * rank_i)
+          #weightUpdates[i] *= np.exp(-self.learningRate * np.sum(delta * rank_i))
+
+          #gradw[i] += np.sum(delta * rank_i)
+
+          #objF += self.weights[i] * np.sum(delta_i)
+          #objF += self.weights[i] * ktau
+
+      #for i in range(numMetrics):
+      #  weightUpdates[i] *= np.exp(-self.learningRate * gradw[i])
       
       prevWeights = self.weights
 
       self.weights = weightUpdates / weightUpdates.sum()
+
+      #print "Objective", optIter, "=", objF
 
       if np.max(np.abs(self.weights - prevWeights)) < self.tolerance: 
         break
@@ -171,7 +228,9 @@ class UnsupervisedLearningRankAggregator:
 
       Ri = self.get_rank_vector(metricValues)
       Ri[np.isnan(Ri)] = len(metricValues)
-      Ri[Ri > self.threshold] = self.threshold + 1
+
+      # Only do thresholding for updates, allow ranking to go beyond top-k
+      #Ri[Ri > self.threshold] = self.threshold + 1
 
       Rweighted += self.weights[i] * Ri
 
@@ -184,6 +243,8 @@ class UnsupervisedLearningRankAggregator:
 
 if __name__ == "__main__":
 
+  np.random.seed(78261310579)
+
   trueRank = np.random.permutation(20)
 
   print "True rank", trueRank
@@ -191,7 +252,7 @@ if __name__ == "__main__":
   numSamples = len(trueRank)
   numMetrics = 5
 
-  numSubSamples = int(numSamples * 0.8)
+  numSubSamples = int(numSamples * 0.6)
 
   metricValues = np.zeros((numSamples, numMetrics), np.float64)
 
@@ -201,10 +262,11 @@ if __name__ == "__main__":
     metricValues[:,i] = np.round(x)
   metricValues[:,-1] = np.round( np.random.rand(numSamples) * 100 )
 
-  # Create outlier
-  #metricValues[7,1] = 20000
+  # Replace entries with outliers or missing values
   metricValues[7,0] = np.nan
   metricValues[10,2] = np.nan
+  #metricValues[7,0] = 20000
+  #metricValues[10,2] = 20000
 
   metricOrder = np.ones(numMetrics)
 
@@ -212,7 +274,7 @@ if __name__ == "__main__":
 
   averageWeights = np.zeros(numMetrics, np.float64)
 
-  numPerturbations = 20
+  numPerturbations = 100
 
   for t in range(numPerturbations):
     sampleInd = np.random.permutation(numSamples)[:numSubSamples]
@@ -251,7 +313,7 @@ if __name__ == "__main__":
   print "ranking(x)", rankagg.get_rank_vector(x)
   """
 
-  print "True rank\n", np.float64(trueRank)
+  print "True rank\n", np.float64(trueRank) + 1
 
   print "Aggregated rank with estimated weights\n", rankagg.get_aggregated_rank(metricValues, metricOrder)
 
--- Python/RankAggregation/computeRankWeights.py
+++ Python/RankAggregation/computeRankWeights.py
@@ -20,11 +20,12 @@ import scipy.stats
 
 import os, sys
 import glob
-
 import random
+import time
 
 import multiprocessing
 
+import json
 import shutil
 import subprocess
 import tempfile
@@ -77,10 +78,12 @@ if __name__ == "__main__":
     description="Compute metric weights for ranking submissions in an image segmentation challenge")
   parser.add_argument("binaryPath", type=str,
     help="path containing perturbation and metric binaries")
-  parser.add_argument("groundTruthPath", type=str,
-    help="path containing ground truth files")
+  parser.add_argument("groundTruthRootPath", type=str,
+    help="path containing folders with ground truth from different raters")
   parser.add_argument("submissionsRootPath", type=str,
     help="path containing folders for different submissions")
+  parser.add_argument("weightsFile", type=str,
+    help="file for storing competition weights in JSON format")
   parser.add_argument("-p", "--perturbations", type=int, default=100,
     help="number of perturbations per ground truth")
   parser.add_argument("-t", "--threads", type=int, default=8,
@@ -89,8 +92,10 @@ if __name__ == "__main__":
   args = parser.parse_args()
 
   binaryPath = args.binaryPath
-  groundTruthPath = args.groundTruthPath
+  groundTruthRootPath = args.groundTruthRootPath
   submissionsRootPath = args.submissionsRootPath
+
+  weightsFile = args.weightsFile
 
   numPerturbations = args.perturbations
   numProcesses = args.threads
@@ -99,6 +104,9 @@ if __name__ == "__main__":
   if numProcesses > numCPU or numProcesses < 1:
     print "Setting number of processes from", numProcesses, "to", numCPU
     numProcesses = numCPU
+
+  # Limit number of ITK threads during metric evals and perturbations
+  os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = str(8)
 
   print "Applying", numPerturbations, "perturbations per ground truth"
   print "Using", numProcesses, "processes"
@@ -111,7 +119,15 @@ if __name__ == "__main__":
   random.seed(2790150234680)
   np.random.seed(78261310579)
 
+  startTime = time.clock()
+
   metricBinaryList = sorted(glob.glob(os.path.join(binaryPath, "ValidateImage*")))
+
+  # Remove Jaccard if we have both Dice and Jaccard (since they're equivalent)
+  diceBinary = os.path.join(binaryPath, "ValidateImageDice")
+  jaccardBinary = os.path.join(binaryPath, "ValidateImageJaccard")
+  if diceBinary in metricBinaryList and jaccardBinary in metricBinaryList:
+    metricBinaryList.remove(jaccardBinary)
 
   # DEBUG
   debugAddRandom = False
@@ -120,7 +136,17 @@ if __name__ == "__main__":
     print "No image metric apps detected"
     sys.exit(-1)
 
-  numMetrics = len(metricBinaryList)
+  numMetricBinaries = len(metricBinaryList)
+
+  metricIDDict = dict()
+  metricIDDict["ValidateImageAveDist"] = "Adb"
+  metricIDDict["ValidateImageDice"] = "Dice"
+  metricIDDict["ValidateImageHausdorffDist"] = "Hdb"
+  metricIDDict["ValidateImageJaccard"] = "Jac"
+  metricIDDict["ValidateImageKappa"] = "Kap"
+  metricIDDict["ValidateImagePPV"] = "PPV"
+  metricIDDict["ValidateImageSpecificity"] = "Spec"
+  metricIDDict["ValidateImageSensitivity"] = "Sens"
 
   perturbBinaryList = glob.glob(os.path.join(binaryPath, "PerturbImageLabels*"))
 
@@ -133,36 +159,49 @@ if __name__ == "__main__":
   # Parameters for different perturbation binaries
   perturbParameterDict = dict()
   perturbParameterDict["PerturbImageLabelsMorphology"] = \
-    ["--iterations", "2", "--radius", "1"]
+    ["--iterations", "4", "--radius", "1"]
   perturbParameterDict["PerturbImageLabelsAffine"] = \
-    ["--maxScaleFactor", "1.02", "--maxRotationAngle", "4", "--maxTranslation", "2" ]
+    ["--maxScaleFactor", "1.05", "--maxRotationAngle", "15", "--maxTranslation", "4" ]
   perturbParameterDict["PerturbImageLabelsBSpline"] = \
-    ["--gridNodes", "8", "--normalVariance", "1.5"]
+    ["--gridNodes", "8", "--normalVariance", "4.0"]
 
   # Get ground truth files
-  groundTruthFiles = \
-    [f for f in sorted(glob.glob(os.path.join(groundTruthPath, "*"))) \
-     if os.path.isfile(f)]
+  groundTruthFilesTable = []
+  for p in sorted(glob.glob(os.path.join(groundTruthRootPath, "*"))):
+    if os.path.isdir(p):
 
-  numGroundTruthSamples = len(groundTruthFiles)
+      groundTruthFiles = \
+        [f for f in sorted(glob.glob(os.path.join(p, "*"))) \
+        if os.path.isfile(f)]
 
-  numGroundTruthSubSamples = int(numGroundTruthSamples * 0.8)
+      groundTruthFilesTable.append(groundTruthFiles)
+
+  # Find number of samples for ground truth, and the rater with the complete set
+  numGroundTruthSamples = 0
+
+  groundTruthCompleteFiles = []
+
+  for files in groundTruthFilesTable:
+    if len(files) > numGroundTruthSamples:
+      numGroundTruthSamples = len(files)
+      groundTruthCompleteFiles = files
 
   # Compute number of objects in ground truth set
   numObjects = 0
-  for f in groundTruthFiles:
-    labelImage = sitk.ReadImage(f)
-    labelArray = sitk.GetArrayFromImage(labelImage)
-    maxLabel = np.max(labelArray)
-    if maxLabel > numObjects:
-      numObjects = maxLabel
+  for files in groundTruthFilesTable:
+    for f in files:
+      labelImage = sitk.ReadImage(f)
+      labelArray = sitk.GetArrayFromImage(labelImage)
+      maxLabel = np.max(labelArray)
+      if maxLabel > numObjects:
+        numObjects = maxLabel
 
   print "Analyzing", numObjects, "objects in all label images"
 
   # Order of metric for ranking (high is good = 1, high is bad = -1)
   # Distance measures are marked negative
   metricOrder = []
-  for i in range(numMetrics):
+  for i in range(numMetricBinaries):
     if metricBinaryList[i].find("Kappa") >= 0:
       metricOrder += [1] # Only one value for entire object set
     elif metricBinaryList[i].find("Dist") >= 0:
@@ -177,7 +216,7 @@ if __name__ == "__main__":
 
   # Name of each element in evaluation (by metric-object)
   metricNames = []
-  for i in range(numMetrics):
+  for i in range(numMetricBinaries):
     m = os.path.basename(metricBinaryList[i])
     m = m[len("ValidateImage"):]
     if m.find("Kappa") >= 0:
@@ -199,14 +238,14 @@ if __name__ == "__main__":
 
       # Select files that correspond to a ground truth
       submissionFiles = []
-      for f in sorted(glob.glob(os.path.join(submissionsRootPath, p, "*"))):
+      for f in sorted(glob.glob(os.path.join(p, "*"))):
         if not os.path.isfile(f):
           continue
 
         q = getSubjectKey(f)
 
         match = False
-        for t in groundTruthFiles:
+        for t in groundTruthCompleteFiles:
           if os.path.basename(t).find(q) >= 0:
             match = True
             break
@@ -233,12 +272,26 @@ if __name__ == "__main__":
 
   for t in range(numPerturbations):
 
+    # Cycle between different raters
+    groundTruthFiles = \
+      groundTruthFilesTable[t % len(groundTruthFilesTable)]
+
+    numGroundTruthSamples = len(groundTruthFiles)
+
+    numGroundTruthSubSamples = int(numGroundTruthSamples * 0.6)
+
     # Pick a type of perturbation
+
+    """
     i_pert = np.random.randint(0, numPerturbers+1)
     if i_pert < numPerturbers:
       perturber = perturbBinaryList[i_pert]
     else:
       perturber = "PassThrough"
+    """
+
+    i_pert = np.random.randint(0, numPerturbers)
+    perturber = perturbBinaryList[i_pert]
 
     print "Applying", os.path.basename(perturber)
 
@@ -248,7 +301,7 @@ if __name__ == "__main__":
 
     def getMetricTable(gt):
 
-      print "Examining ground truth:", os.path.basename(gt)
+      print "Examining ground truth:\n", gt
 
       pname = multiprocessing.current_process().name
 
@@ -300,7 +353,7 @@ if __name__ == "__main__":
 
         # Evaluate each metric on gt and perturbed submission
         metricValues = []
-        for j in range(numMetrics):
+        for j in range(numMetricBinaries):
 
           # Run validation app and obtain list of metrics from stdout
           command = [metricBinaryList[j], pert_gt, subm, textout]
@@ -353,7 +406,7 @@ if __name__ == "__main__":
 
     averageWeights += w
 
-    print "Average weights\n", averageWeights/(t+1)
+    print "Average weights after", t+1, "perturbations\n", averageWeights/(t+1)
 
   averageWeights /= numPerturbations
 
@@ -363,30 +416,91 @@ if __name__ == "__main__":
 
   print "Sum weights = ", np.sum(averageWeights)
 
-  print "Metric names:\n", metricNames
-  print "Average weights =\n", averageWeights
+  #print "Metric names:\n", metricNames
+  #print "Average weights =\n", averageWeights
 
+  print "Metric weights:"
   for i in range(len(metricNames)):
     print metricNames[i], "->", averageWeights[i]
+  print "--------------------"
 
-  imin = np.argmin(averageWeights)
-  print "Smallest weight is", averageWeights[imin], "for", metricNames[imin]
-  imax = np.argmax(averageWeights)
-  print "Largest weight is", averageWeights[imax], "for", metricNames[imax]
+  #imin = np.argmin(averageWeights)
+  #print "Smallest weight is", averageWeights[imin], "for", metricNames[imin]
+  #imax = np.argmax(averageWeights)
+  #print "Largest weight is", averageWeights[imax], "for", metricNames[imax]
 
-  # Store average weights for future evaluation
+  print "Sorted metric weights (largest to smallest):"
+  isort = np.argsort(averageWeights * -1.0)
+  for i in isort:
+    print metricNames[i], "->", averageWeights[i]
+  print "--------------------"
+
+  print "Sum of weights by metric type:"
+  metricTypeWeights = dict()
+  for i in range(len(metricNames)):
+    t = metricNames[i].split("_")[0]
+    if not t in metricTypeWeights:
+      metricTypeWeights[t] = averageWeights[i]
+    else:
+      metricTypeWeights[t] += averageWeights[i]
+  for k in sorted(metricTypeWeights.keys()):
+    print k, "->", metricTypeWeights[k]
+  print "--------------------"
+
+  print "Sum of weights by object:"
+  objectWeights = dict()
+  for i in range(len(metricNames)):
+    t = metricNames[i].split("_")[1]
+    if not t in objectWeights:
+      objectWeights[t] = averageWeights[i]
+    else:
+      objectWeights[t] += averageWeights[i]
+  for k in sorted(objectWeights.keys()):
+    print k, "->", objectWeights[k]
+  print "--------------------"
+
+  # Store final weights for future evaluation
+
+  """
   with open("metricNames.txt", 'w') as file:
     for item in metricNames:
       file.write("%s\n" % item)
 
   np.save("metric_weights.npy", averageWeights)
+  """
+
+  competitionWeights = dict()
+  for i in range(len(metricNames)):
+    name_obj = metricNames[i].split("_")
+    name = name_obj[0]
+    obj = name_obj[1][len('object'):]
+
+    binaryName = "ValidateImage" + name
+
+    if binaryName in metricIDDict:
+      id = metricIDDict[binaryName] + obj
+    else:
+      id = name + obj
+
+    if obj == "":
+      obj = "all"
+
+    # Assign ordered weights to each metric ID
+    competitionWeights[id] = \
+      { \
+        "title":name + " (label " + obj +")", \
+        "weight":(averageWeights[i] * metricOrder[i]) \
+      }
+
+  with open(weightsFile, "w") as outfile:
+    json.dump(competitionWeights, outfile, indent=2, sort_keys=True)
 
   # Apply computed weights to get rankings, averaged over different ground
   # truth cases
 
   def getFinalRank(gt):
 
-    print "Examining ground truth:", os.path.basename(gt)
+    print "Examining ground truth:\n", gt
 
     pname = multiprocessing.current_process().name
 
@@ -415,7 +529,7 @@ if __name__ == "__main__":
 
       # Evaluate each metric on gt and perturbed submission
       metricValues = []
-      for j in range(numMetrics):
+      for j in range(numMetricBinaries):
 
         # Run validation app and obtain list of metrics from stdout
         command = [metricBinaryList[j], gt, subm, textout]
@@ -442,7 +556,9 @@ if __name__ == "__main__":
 
   pool = multiprocessing.Pool(processes=numProcesses)
 
-  finalRankTable = pool.map(getFinalRank, groundTruthFiles)
+  # TODO: compute final rank using multiple tables from multiple raters
+  # TODO: move this to a separate script? applyRankWeights.py?
+  finalRankTable = pool.map(getFinalRank, groundTruthCompleteFiles)
 
   pool.close()
   pool.join()
@@ -452,7 +568,7 @@ if __name__ == "__main__":
     finalAggregatedRank += rank_gt
 
   finalAggregatedRank /= numGroundTruthSamples
-  finalAggregatedRank = scipy.stats.rankdata(finalAggregatedRank) - 1
+  finalAggregatedRank = scipy.stats.rankdata(finalAggregatedRank)
 
   print "Final aggregated rank (over all ground truth)\n", finalAggregatedRank
 
@@ -471,3 +587,5 @@ if __name__ == "__main__":
   print "Clean up"
   for f in tempFileSet:
     os.remove(f)
+
+  print "Rank aggregation took", time.clock() - startTime, "seconds"
